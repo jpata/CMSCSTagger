7318b7bec944159490b7e2da7df0cb800d446be8
Joosep Pata <joosep.pata@cern.ch>
2015-04-07 18:09:57 +0300

    added pt/eta bin categorization

--- python/dumper2.py
+++ python/dumper2.py
@@ -1,6 +1,7 @@
 import ROOT, sys
 from treeStructure import *
-
+import math
+import numpy as np
 from array import array
 
 class NTupleVariable:
@@ -9,10 +10,10 @@ class NTupleVariable:
         self.func = kwargs.get("func")
         self.dtype = kwargs.get("dtype", "f")
         self.var = array(self.dtype, [0.0])
-        
+
     def set(self, x):
         self.var[0] = self.func(x)
-    
+
     def create_branch(self, tree):
         tree.Branch(
             self.name, self.var,
@@ -30,10 +31,21 @@ jet_pt = NTupleVariable(
     func=lambda x: x.pt
 )
 
+jet_pt_bin = NTupleVariable(
+    name="pt_bin",
+    func=lambda x: x.pt_bin
+)
+
 jet_eta = NTupleVariable(
     name="eta",
     func=lambda x: x.eta
 )
+
+jet_eta_bin = NTupleVariable(
+    name="eta_bin",
+    func=lambda x: x.eta_bin
+)
+
 
 jet_csv1 = NTupleVariable(
     name="csv1",
@@ -77,7 +89,7 @@ jet_sv_flight2dsig = NTupleVariable(
 )
 
 
-ofile = ROOT.TFile("out.root", "RECREATE")
+ofile = ROOT.TFile(sys.argv[2], "RECREATE")
 otree = ROOT.TTree("tree", "tree")
 otree.SetDirectory(ofile)
 
@@ -91,6 +103,9 @@ registered_branches_jet = [
 for br in registered_branches_jet:
     br.create_branch(otree)
 
+pt_bins = np.linspace(20, 820, 81)
+eta_bins = np.linspace(0.0, 2.5, 21)
+
 n = tt.GetEntries()
 print n
 for i in range(n):
@@ -98,32 +113,35 @@ for i in range(n):
         print i
     tt.GetEntry(i)
     ev = Event(tt)
-    
+
     svs = ev.SV
     for ijet, jet in enumerate(ev.Jet):
-        
+        if jet.pt < 20:
+            continue
+        jet.pt_bin = pt_bins.searchsorted(jet.pt)
+        jet.eta_bin = eta_bins.searchsorted(abs(jet.eta))
         jet.sv_chi2ndf = 0.0
         jet.sv_flight3dsig = 0.0
         jet.sv_flight2dsig = 0.0
-        
+
         nsv_first = jet.nFirstSV
         nsv_last = jet.nLastSV
-        
+
         jet_svs = sorted(
             svs[nsv_first:nsv_last],
             key=lambda x: x.chi2 / x.ndf
         )
         jet.nsvs = len(jet_svs)
-        
+
         if len(jet_svs)>0:
             jet.sv_chi2ndf = jet_svs[0].chi2 / jet_svs[0].ndf
             jet.sv_flight3dsig = jet_svs[0].flight / jet_svs[0].flightErr
             jet.sv_flight2dsig = jet_svs[0].flight2D / jet_svs[0].flight2DErr
-                
+
         for br in registered_branches_jet:
             br.set(jet)
-            
+
         otree.Fill()
-    
+
 ofile.Write()
 ofile.Close()
