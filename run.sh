3a841da8771f0b197cd71e82be7b2ad552f7e4e0
Joosep Pata <joosep.pata@cern.ch>
2015-05-21 15:23:27 +0300

    uncomment

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ python python/project_bins.py tree_c test/qcd.root qcd &
 python python/project_bins.py tree_l test/qcd.root qcd &
 wait
 
-#for x in training testing rest; do
-#    for y in ttjets qcd; do
-#        for z in b c l; do
-#            #echo "$x $y $z"
-#            hadd -f ${y}_${z}_${x}.root ${y}_tree_${z}_${x}_*.root
-#        done
-#    done
-#done
+for x in training testing rest; do
+    for y in ttjets qcd; do
+        for z in b c l; do
+            #echo "$x $y $z"
+            hadd -f ${y}_${z}_${x}.root ${y}_tree_${z}_${x}_*.root
+        done
+    done
+done
