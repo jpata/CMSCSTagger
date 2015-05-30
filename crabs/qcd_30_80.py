31d50a9a48fe131c954bb4965fe0e4e79567b761
Joosep Pata <joosep.pata@cern.ch>
2015-05-30 12:08:30 +0200

    updated crabs with whitelist

--- crabs/qcd_30_80.py
+++ crabs/qcd_30_80.py
@@ -6,12 +6,11 @@ config = Configuration()
 
 config.section_("General")
 config.General.transferLogs = True
-config.General.requestName = "btagana_qcd_30_80_v2"
+config.General.requestName = "btagana_qcd_30_80_May29_v1"
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.allowNonProductionCMSSW = True
-config.JobType.maxJobRuntimeMin = 60*48 #maximal job runtime in minute
+#config.JobType.maxJobRuntimeMin = 60*48 #maximal job runtime in minute
 config.JobType.psetName = "../python/runAnalyzerAOD.py"
 
 config.section_("Data")
@@ -20,10 +19,10 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.publication = False
 
-config.Data.unitsPerJob = 10
+config.Data.unitsPerJob = 20
 config.Data.totalUnits = 2000
 
 config.section_("Site")
 config.Site.storageSite = "T2_EE_Estonia"
-
+config.Site.whitelist = ["T2_BE_IIHE"]
 
