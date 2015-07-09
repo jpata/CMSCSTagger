ad4859361a9592c8d793284b1b1c3cc10f3f2c2e
Joosep <joosep.pata@cern.ch>
2015-07-09 09:30:32 +0200

    cleaned up eth computer

--- plugins/CombinedMVAJetTagESProducerETH.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/Framework/interface/ModuleFactory.h"
-
-#include "RecoBTau/JetTagComputer/interface/JetTagComputerESProducer.h"
-#include "RecoBTag/CombinedMVA/interface/CombinedMVAJetTagComputerETH.h"
-
-typedef JetTagComputerESProducer<CombinedMVAJetTagComputerETH> CombinedMVAJetTagESProducerETH;
-DEFINE_FWK_EVENTSETUP_MODULE(CombinedMVAJetTagESProducerETH);
